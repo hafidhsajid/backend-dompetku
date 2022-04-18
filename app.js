e7dc4233964e5dc4b927d4d99c64173713a54a6e
Hafidh Sajid <hafidhsajid@gmail.com>
2022-04-19 05:27:26 +0700

    delete

--- app.js
+++ app.js
@@ -44,6 +44,35 @@ app.get('/api/history', (req, res) => {
         res.status(200).json({ success: true, data: rows });
     });
 });
+app.delete('/api/history/:id', (req, res) => {
+    // buat query sql untuk mencari data dan hapus
+    const querySearch = 'SELECT * FROM history WHERE id = ?';
+    const queryDelete = 'DELETE FROM history WHERE id = ?';
+
+    // jalankan query untuk melakukan pencarian data
+    koneksi.query(querySearch, req.params.id, (err, rows, field) => {
+        // error handling
+        if (err) {
+            return res.status(500).json({ message: 'Ada kesalahan', error: err });
+        }
+
+        // jika id yang dimasukkan sesuai dengan data yang ada di db
+        if (rows.length) {
+            // jalankan query delete
+            koneksi.query(queryDelete, req.params.id, (err, rows, field) => {
+                // error handling
+                if (err) {
+                    return res.status(500).json({ message: 'Ada kesalahan', error: err });
+                }
+
+                // jika delete berhasil
+                res.status(200).json({ success: true, message: 'Berhasil hapus data!' });
+            });
+        } else {
+            return res.status(404).json({ message: 'Data tidak ditemukan!', success: false });
+        }
+    });
+});
 
 // buat server nya
 app.listen(PORT, () => console.log(`Server running at port: ${PORT}`));
