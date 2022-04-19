c04ff4fedcd0fc7d752fef4281a427b8ef1e532c
Hafidh Sajid <hafidhsajid@gmail.com>
2022-04-19 22:43:14 +0700

    database bener

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const koneksi = require('./database1');
+const koneksi = require('./database');
 const app = express();
 const PORT = process.env.PORT || 5000;// set body parser
 
